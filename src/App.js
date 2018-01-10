e5d734fc4873b26a233b8bed8cab80b7d5795da2
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-01-10 01:55:56 -0500

    app.js changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import "./materialize/css/materialize.css"
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Score from './components/Score'
+import MainPart from './components/MainPart'
 
 class App extends Component {
   render() {
---
+++ src/components/Score.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../materialize/css/materialize.css"
+
+// class Header extends Component {
+// 	render() {
+// 		return (
+// 			<h1>Helelo Vitaliy</h1>
+// 			);
+// 	}
+// }
+
+const Score = ()=>	<h1>Hello Score</h1>
+export default Score;
