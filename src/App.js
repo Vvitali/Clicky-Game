67caf8042c6b1589b2a04ca2d6f69005237fa768
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-01-10 01:54:05 -0500

    init, components, materialize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import "./materialize/css/materialize.css"
+import Header from './components/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <Header>1</Header>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../materialize/css/materialize.css"
+
+// class Header extends Component {
+// 	render() {
+// 		return (
+// 			<h1>Helelo Vitaliy</h1>
+// 			);
+// 	}
+// }
+
+const Header = ()=>	<h1>Hello Test</h1>
+export default Header;
