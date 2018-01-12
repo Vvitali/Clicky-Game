c60c91055426449aeb34a793fc8b1216cb78e66d
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-01-12 02:16:58 -0500

    basic score added

--- src/components/MainPart.js
+++ src/components/MainPart.js
@@ -5,7 +5,7 @@ import GameItems from "./GameItem";
 class MainPart extends Component{
 	state = {
 		score: 0,
-		totalScore: 0,
+		bestScore: 0,
 		clickedChars:{"bb-8": false
 		, "boba-fett": false
 		, "c-3po": false
@@ -22,46 +22,34 @@ class MainPart extends Component{
 
 	constructor(props){
 		super(props);
-		this.clickHandlerS = this.clickHandlerS.bind(this);
 		this.guessChar = this.guessChar.bind(this);
 	}
-
-	//eventHandler for button
-	clickHandlerS = event =>{
-		console.log(event.target.id)
-		//if not use this line - react CLI gives you a warning about direct usage of states
-		let score = this.state.score+1;
-		this.setState({
-			[event.target.id]: score
-		});
-	};
 
 	guessChar = event =>{
 		let char = event.target.getAttribute("char");
 		var updatedState = this.state.clickedChars;
-		this.state.clickedChars[char]? console.log("You lose!") : console.log("You win!");
+		this.state.clickedChars[char]? console.log("You lose!") : console.log(char+" ok");
 		updatedState[char]=true;
+		let score = this.state.score+1;
 		this.setState({
-			clickedChars : updatedState
+			clickedChars : updatedState, score: score
 		});
-		console.log(this.state.clickedChars[char]);
 	};
 
 	render(){
 		return (
 			<main className="container">
 			<div className="row">
-			<div className="col s12 m10">
+			<div className="col s12 m9">
 			<div className="row">
-			<h1 id="1" key="asda">Hello Main Part {this.state.score}</h1>
-			<button className="btn" id="score" onClick={this.clickHandlerS}>Click me!</button>
+			<h1 id="1" key="asda">Hello %username%</h1>
 			</div>
 			<h2>Images:</h2>
 			<GameItems onClick={this.guessChar}></GameItems>
 
 			</div>
-			<div  className="col m2">
-			<Score></Score></div>
+			<div className="col m3">
+			<Score score={this.state.score}></Score></div>
 			</div>
 			</main>
 			);
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {Component} from "react";
 
+class Score extends Component{
+	render(){
+		return <div><h4>Score: {this.props.score}</h4><h4>Best score:</h4></div>
+	}
+}
 
-const Score = ()=> <h3>Hello Score</h3>
 export default Score;
