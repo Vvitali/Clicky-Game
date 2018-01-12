88cf43728c79348f09632cd6aa0806fa6123e5a6
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-12 11:23:26 -0500

    new header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,9 @@ import React from 'react';
 import "./footer.css"
 
 const Footer = ()=>	{
-	return <footer className="page-footer blue">
-	<div className="container">
-	<div className="row">
-	<div className="col l6 s12">
-	<h5 className="white-text">Footer Content</h5>
-	<p className="grey-text text-lighten-4">Organize your footer content.</p>
-	</div>
-	</div>
-	</div>
-	<div className="footer-copyright">
-	<div className="container">
-	© 2018 Copyright Text
-	<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-	</div>
+	return <footer class="page-footer blue">
+	<div class="footer-copyright center-align">
+	<div class="container">© 2018 Vvitali</div>
 	</div>
 	</footer>
 
